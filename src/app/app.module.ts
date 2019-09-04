e532496afa3098e54eae314b717ce174806eb281
krishnakantsalkar <krishnakantsalkar@gmail.com>
2019-09-04 15:44:46 +0530

    #Angular8: Parent Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CourseComponent } from "./components/course/course.component";
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent],
+  declarations: [AppComponent, CourseComponent, ParentComponent, ChildComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
